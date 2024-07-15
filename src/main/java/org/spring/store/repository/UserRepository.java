7511c3f2c13d8e0c33864e56c5293da2313bb34a
Vika <v.yanulevich@bk.ru>
2024-07-15 22:33:39 +0300

    update UserRepository

--- src/main/java/org/spring/store/repository/UserRepository.java
+++ src/main/java/org/spring/store/repository/UserRepository.java
@@ -1,13 +1,17 @@
 package org.spring.store.repository;
 
 import org.spring.store.model.User;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
+    List<User> findAll(Sort sort);
+    List<User> findByEmailContaining(String email);
 }
